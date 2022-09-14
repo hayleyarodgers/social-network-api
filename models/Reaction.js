da75e3cc2c5b7f76a095b23f6d23163e1d000e2f
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-09-14 14:28:59 +1000

    Write schema for reaction subdocument

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			maxLength: 280,
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: formatDate,
+		},
+	},
+	{
+		toJSON: {
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+const formatDate = (createdAt) => {
+	return createdAt.toLocaleDateString();
+};
+
+module.exports = reactionSchema;
