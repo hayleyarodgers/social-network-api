2673bf4725e614c330de5ad4f038498e7cb31d89
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-09-14 15:11:16 +1000

    Write schema and initialise User model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			// User regex to validate email
+			match: [
+				/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+				"Please add a valid email address.",
+			],
+		},
+		// Array of ids of thoughts created by a user
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "thought",
+			},
+		],
+		// Array of ids of user's friends
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "user",
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
+
+// Create a virtual property "friendCount" that gets the number of friends a user has
+thoughtSchema.virtual("friendCount").get(function () {
+	return this.friends.length;
+});
+
+// Initialise User model
+const User = model("user", userSchema);
+
+module.exports = User;
